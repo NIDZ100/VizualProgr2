6ab4b46677c3fb077b5553fa5166422628a4a15b
NIDZ100 <xiamuser11@gmail.com>
2024-11-14 16:18:38 +0200

    Uzdevumi līdz 6.5(ieskaitot).3.uzd.

---
+++ Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Lab2.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Lab3.WpfApplication.ViewModels
+{
+    public class MainWindowViewModel: ObservableObject
+    {
+       
+            private OwnerDbContext _db;
+
+
+            public MainWindowViewModel()
+            {
+                _db = new OwnerDbContext();
+            }
+            private Owner[] _owners;
+            public Owner[] Owners => _owners;
+
+            public void Load()
+            {
+                _owners = _db.Owners.ToArray();
+            }
+        }
+    }
+
+
