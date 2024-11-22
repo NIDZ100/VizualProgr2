ff810ff5a3cd31a3cdcdd5bcb36f1e5da531d8dc
NIDZ100 <xiamuser11@gmail.com>
2024-11-22 17:14:50 +0200

    Ir visi uzdevumi uz 10 (lab.d. 3)

--- Lab3.WpfApplication/MainWindow.xaml.cs
+++ Lab3.WpfApplication/MainWindow.xaml.cs
@@ -1,28 +1,12 @@
 ﻿using Lab3.WpfApplication.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab3.WpfApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-
         private MainWindowViewModel _viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,7 +19,5 @@ namespace Lab3.WpfApplication
             _viewModel.Load();
             this.DataContext = _viewModel;
         }
-        
-       
     }
 }
--- Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
+++ Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
@@ -1,68 +1,199 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Lab2.DataAccess;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.Input;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace Lab3.WpfApplication.ViewModels
 {
-    public class MainWindowViewModel: ObservableObject , INotifyPropertyChanged 
+    public class MainWindowViewModel : ObservableObject
     {
         private List<Animal> _animals = new List<Animal>();
+        private Owner[] _owners;
+        private string _searchDescription;
         private Owner _selectedOwner;
+        private Owner _editableOwner;
+        private string _selectedIdRange;
+        private readonly OwnerDbContext _db;
 
         public List<Animal> Animals
         {
-            get
-            {
-                return _animals;
-            }
+            get => _animals;
             set
             {
                 _animals = value;
                 OnPropertyChanged();
             }
         }
-       
-        public Owner SelectedOwner { get; set; } 
-        private OwnerDbContext _db;
+
+        public Owner[] Owners
+        {
+            get => _owners;
+            set
+            {
+                _owners = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SearchDescription
+        {
+            get => _searchDescription;
+            set
+            {
+                _searchDescription = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Owner SelectedOwner
+        {
+            get => _selectedOwner;
+            set
+            {
+                _selectedOwner = value;
+                OnPropertyChanged();
+                LoadAnimals();
+                EditableOwner = _selectedOwner != null ? new Owner
+                {
+                    Id = _selectedOwner.Id,
+                    Surname = _selectedOwner.Surname,
+                    Height = _selectedOwner.Height
+                } : null;
+                RaiseCanExecuteChanged(); 
+            }
+        }
+
+        public Owner EditableOwner
+        {
+            get => _editableOwner;
+            set
+            {
+                _editableOwner = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> IdRanges { get; } = new() { "1-10", "10-20", "20+" };
+
+        public string SelectedIdRange
+        {
+            get => _selectedIdRange;
+            set
+            {
+                _selectedIdRange = value;
+                OnPropertyChanged();
+                FilterData(); 
+            }
+        }
+
+        public ICommand SelectOwnerCommand { get; }
+        public ICommand SearchCommand { get; }
+        public ICommand DeleteOwnerCommand { get; }
+        public ICommand UpdateOwnerCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            _db = new OwnerDbContext();
+            SelectOwnerCommand = new RelayCommand(SelectOwner);
+            SearchCommand = new RelayCommand(FilterData);
+            DeleteOwnerCommand = new RelayCommand(DeleteOwner, CanDeleteOwner);
+            UpdateOwnerCommand = new RelayCommand(UpdateOwner, CanUpdateOwner);
+            Load(); 
+        }
+
+        public void Load()
+        {
+            Owners = _db.Owners.ToArray();
+        }
 
         public void LoadAnimals()
         {
             if (SelectedOwner == null)
             {
+                Animals = new List<Animal>();
                 return;
             }
-            Animals = _db.Animals.Where(o => o.Owner.Id == SelectedOwner.Id).ToList();
+
+            Animals = _db.Animals.Where(a => a.Owner.Id == SelectedOwner.Id).ToList();
         }
-        public MainWindowViewModel()
-            {
-                _db = new OwnerDbContext();
-                SelectOwnerCommand = new RelayCommand(LoadAnimals);
-            }
-            private Owner[] _owners;
-            public Owner[] Owners => _owners;
 
-            public void Load()
-            {
-                _owners = _db.Owners.ToArray();
-            }
-        public ICommand SelectOwnerCommand { get; }
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        public void FilterData()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-        
+            if (SelectedOwner == null)
+            {
+                return; 
+            }
 
+            var query = _db.Animals.Where(a => a.Owner.Id == SelectedOwner.Id);
+
+            
+            if (!string.IsNullOrWhiteSpace(SearchDescription))
+            {
+                query = query.Where(a => a.Description.ToLower().Contains(SearchDescription.ToLower()));
+            }
+
+           
+            if (!string.IsNullOrEmpty(SelectedIdRange))
+            {
+                query = SelectedIdRange switch
+                {
+                    "1-10" => query.Where(a => a.Id >= 1 && a.Id <= 10),
+                    "10-20" => query.Where(a => a.Id > 10 && a.Id <= 20),
+                    "20+" => query.Where(a => a.Id > 20),
+                    _ => query
+                };
+            }
+
+            Animals = query.ToList();
+        }
+
+        private void SelectOwner()
+        {
+            
+            if (SelectedOwner != null)
+            {
+                LoadAnimals();
+            }
+        }
+
+        private bool CanDeleteOwner() => SelectedOwner != null;
+
+        private void DeleteOwner()
+        {
+            if (SelectedOwner == null) return;
+
+            
+            var animalsToDelete = _db.Animals.Where(a => a.Owner.Id == SelectedOwner.Id).ToList();
+            _db.Animals.RemoveRange(animalsToDelete);
+            _db.Owners.Remove(SelectedOwner);
+            _db.SaveChanges();
+
+            Load();
+            Animals = new List<Animal>();
+        }
+
+        private bool CanUpdateOwner() => SelectedOwner != null;
+
+        private void UpdateOwner()
+        {
+            if (SelectedOwner == null) return;
+
+           
+            SelectedOwner.Surname = EditableOwner.Surname;
+            SelectedOwner.Height = EditableOwner.Height;
+
+           
+            _db.Update(SelectedOwner);
+            _db.SaveChanges();
+            Load(); 
+        }
+
+        private void RaiseCanExecuteChanged()
+        {
+            (DeleteOwnerCommand as RelayCommand)?.NotifyCanExecuteChanged();
+            (UpdateOwnerCommand as RelayCommand)?.NotifyCanExecuteChanged();
+        }
     }
 }
-
-
